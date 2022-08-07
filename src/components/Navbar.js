d786fc0b0caffd03a9cd9753bed82c964f7b9b6b
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-07 21:37:33 +0100

    bootstrap navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Navbar, Container } from "react-bootstrap";
+
+export const Navbar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href="#about"
+              className={
+                activeLink === "about" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("about")}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              href="#contact"
+              className={
+                activeLink === "contact" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("contact")}
+            >
+              Contact
+            </Nav.Link>
+            <Nav.Link href="#">CV</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
