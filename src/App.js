b0d525dd05193e83a4469952d9896152d9571562
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-08 14:05:57 +0100

    banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { NavBar } from "./components/Navbar";
+import { Banner } from "./components/Banner";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export const App = () => {
-  return <NavBar />;
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />;
+    </div>
+  );
 };
---
+++ src/components/Banner.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = [
+    "Software Engineer",
+    "Full-Stack Web Developer",
+    "Graphic Designer",
+  ];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) setDelta((prevDelta) => prevDelta / 2);
+  };
+  if (!isDeleting && updatedText === fullText) {
+    setIsDeleting(true);
+    setDelta(period);
+  } else if (isDeleting && updatedText === "") {
+    setIsDeleting(false);
+    setLoopNum(loopNum + 1);
+    setDelta(500);
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Hello!</span>
+            <h1>
+              {`I'm Sophia`}
+              <span className="wrap">{text}</span>
+            </h1>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
