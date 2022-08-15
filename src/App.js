067f224bee1d17ac4e31402fd97de1961dd52593
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-15 11:05:16 +0100

    starting again

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-import Projects from "./components/Projects";
 
 const App = () => {
   return (
     <React.Fragment>
       <Navbar />
-      <Banner />
-      <Projects />
     </React.Fragment>
   );
 };
