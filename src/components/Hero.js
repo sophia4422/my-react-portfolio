b62fc1739ebb329c9fcff85e3b69c04c849e0e12
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-15 11:31:19 +0100

    banner bg looks how i want it to

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import fl from "../assets/images/fl.jpg";
 export const Hero = () => {
   return (
     <div className="hero-container">
-      <img src={fl} alt="palm trees" className="" />
+      <img src={fl} alt="palm trees" className="hero-image" />
       <h1> Testing Text </h1>
       <p> Testing Paragraph </p>
     </div>
