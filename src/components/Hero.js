f36669dbf708f9887d07eb6fdc932de4a73fb14a
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-15 12:09:45 +0100

    banner has all items in the center

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,17 @@
 import fl from "../assets/images/fl.jpg";
+import me from "../assets/images/polaroids.png";
 
 export const Hero = () => {
   return (
     <div className="hero-container">
       <img src={fl} alt="palm trees" className="hero-image" />
-      <h1> Testing Text </h1>
-      <p> Testing Paragraph </p>
+      <h1> I'm Sophia </h1>
+      <p> Junior Software Engineer </p>
+
+      <div>
+        {" "}
+        <img src={me} alt="sophia" className="polaroid" />
+      </div>
     </div>
   );
 };
