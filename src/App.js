418f3e3d2d2c5ff980cbc536fcef03a07e21c91e
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-19 10:55:41 +0100

    bought navbar back

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
+
+// import { Hero, Projects, Navbar } from "components";
 
 export const App = () => {
   return (
     <React.Fragment>
       <Hero />
       <Projects />
+      <Navbar />
     </React.Fragment>
   );
 };
