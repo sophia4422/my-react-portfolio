11e935ca0595c15304ff0da322d2c88cbbf5b95b
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-19 14:40:41 +0100

    trying parallax but it doesnt work

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,19 @@
 import fl from "../assets/images/fl.jpg";
 import me from "../assets/images/polaroids.png";
-import Typewriter from "typewriter-effect";
+// import Typewriter from "typewriter-effect";
+import useState from "react";
+import { useEffect } from "react";
 
 export const Hero = () => {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="hero-container">
       <img src={fl} alt="palm trees" className="hero-image" />
@@ -13,7 +24,12 @@ export const Hero = () => {
       <p>Junior Software Engineer</p>
       <div>
         {" "}
-        <img src={me} alt="sophia" className="polaroid" />
+        <img
+          src={me}
+          alt="sophia"
+          className="polaroid"
+          style={{ transform: `translateY(${offsetY * 0.5}px)` }}
+        />
       </div>
     </div>
   );
