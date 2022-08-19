89662570aa1e6042a591017af6563cf2d4523b0b
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-19 11:59:40 +0100

    moving the navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
-import Navbar from "./components/Navbar";
 
 // import { Hero, Projects, Navbar } from "components";
 
 export const App = () => {
   return (
     <React.Fragment>
+      <Navbar />
       <Hero />
       <Projects />
-      <Navbar />
     </React.Fragment>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,21 +3,25 @@ import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
   return (
     <header>
       <h3>Logo</h3>
-      <nav>
+      <nav ref={navRef}>
         <a href="/#">Home</a>
         <a href="/#">My Projects</a>
         <a href="/#">About Me</a>
         <a href="/#">Contact Me</a>
         <a href="/#">CV</a>
 
-        <button>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
         </button>
       </nav>
-      <button>
+      <button className="nav-btn" onClick={showNavbar}>
         <FaBars />
       </button>
     </header>
