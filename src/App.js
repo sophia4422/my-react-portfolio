2509fabb20896150dc01453b0b9e54493cccb513
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-19 10:39:03 +0100

    started projects section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 export const App = () => {
   return (
     <React.Fragment>
       <Hero />
+      <Projects />
     </React.Fragment>
   );
 };
---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+export const Projects = () => {
+  return (
+    <section className="projects-container">
+      <h1>Hello</h1>
+    </section>
+  );
+};
+
+export default Projects;
