9acdbdf4930ba15a9c387a67f76eda75ee00c429
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-22 12:30:13 +0100

    added all icons

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faHtml5,
+  faCss3Alt,
+  faJs,
+  faReact,
+  faNode,
+  faLinkedin,
+  faFigma,
+  faNpm,
+  faSketch,
+  faSlack,
+  faSpotify,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const About = () => {
   return (
@@ -27,6 +41,18 @@ export const About = () => {
       <div className="skills-icons">
         <h2>Add skills logos here</h2>
         <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faHtml5}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faCss3Alt}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faJs}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faReact}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faNode}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faFigma}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faNpm}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faSketch}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faSlack}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faSpotify}></FontAwesomeIcon>
       </div>
     </div>
   );
