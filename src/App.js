79dd0d990c4dc460ecc09eb9ae7e2184b853c167
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-23 14:16:53 +0100

    contact component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 // import { Hero, Projects, Navbar } from "components";
 
@@ -13,6 +14,7 @@ export const App = () => {
       <Hero />
       <Projects />
       <About />
+      <Contact />
     </React.Fragment>
   );
 };
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+export const Contact = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+    </div>
+  );
+};
+
+export default Contact;
