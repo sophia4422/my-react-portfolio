770b4431e46ab547a75dcddec51051501fc8fd90
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-23 14:24:26 +0100

    basic form code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,44 @@
 export const Contact = () => {
   return (
     <div>
-      <h1>Projects</h1>
+      <h1>Contact</h1>
+
+      <div class="form-container">
+        <form class="register-form">
+          {/* Uncomment the next line to show the success message */}
+          {/* <div class="success-message">Success! Thank you for registering</div> */}
+          <input
+            id="first-name"
+            class="form-field"
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+          />
+          {/* Uncomment the next line to show the error message */}
+          {/* <span id="first-name-error">Please enter a first name</span> */}
+          <input
+            id="last-name"
+            class="form-field"
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+          />
+          {/* Uncomment the next line to show the error message */}
+          {/* <span id="last-name-error">Please enter a last name</span> */}
+          <input
+            id="email"
+            class="form-field"
+            type="text"
+            placeholder="Email"
+            name="email"
+          />
+          {/* Uncomment the next line to show the error message */}
+          {/* <span id="email-error">Please enter an email address</span> */}
+          <button class="form-field" type="submit">
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
