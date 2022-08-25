6d9b14461578caa2bac8ddf02fbbc3cbe851f124
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-25 11:24:52 +0100

    started footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 // import { Hero, Projects, Navbar } from "components";
 
@@ -15,6 +16,7 @@ export const App = () => {
       <Projects />
       <About />
       <Contact />
+      <Footer />
     </React.Fragment>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <div className="footer-container">
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
