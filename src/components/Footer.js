7997875dfa007ee674bf6b0b43881cf120f452f7
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-28 12:46:53 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,48 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-scroll";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faCircleChevronUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleChevronUp,
+  faFile,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const Footer = () => {
   return (
     <div className="footer-container">
-      <h1>Footer</h1>
+      <div>
+        <h1 className="footer-title">Created by Sophia with React</h1>
+        <p className="footer-para">
+          Please get in touch, or view my CV, by clicking the icons below.
+        </p>
+        <div className="skills-icons-two">
+          <a
+            href="https://www.linkedin.com/in/sophia-wall/"
+            target="_blank"
+            alt="Linkedin"
+            title="Linkedin"
+          >
+            <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+          </a>
 
-      <a
-        href="https://www.linkedin.com/in/sophia-wall/"
-        target="_blank"
-        alt="Linkedin"
-        title="Linkedin"
-      >
-        <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
-      </a>
-
-      <a
-        href="https://github.com/sophia4422"
-        target="_blank"
-        alt="Github"
-        title="Github"
-      >
-        <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-      </a>
-      <Link to="home" spy={true} smooth={true} offset={-80} duration={500}>
-        <FontAwesomeIcon icon={faCircleChevronUp}></FontAwesomeIcon>
-      </Link>
+          <a
+            href="https://github.com/sophia4422"
+            target="_blank"
+            alt="Github"
+            title="Github"
+          >
+            <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+          </a>
+          <FontAwesomeIcon icon={faFile}></FontAwesomeIcon>
+          <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+        </div>
+        <Link to="home" spy={true} smooth={true} offset={-80} duration={500}>
+          <FontAwesomeIcon
+            icon={faCircleChevronUp}
+            className="up-btn"
+          ></FontAwesomeIcon>
+        </Link>
+      </div>
     </div>
   );
 };
